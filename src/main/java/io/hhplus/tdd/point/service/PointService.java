28a4ae98fe43624929e87e821e6dff5f9bdb0aff
ksb9242 <ksb9242@gmail.com>
2024-12-17 12:43:03 +0900

    [feat] : PointServiceTest - 포인트 조회 단위 테스트 코드 작성

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -1,21 +1,25 @@
 package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
     private final UserPointTable userPointTable;
 
+    // 특정 유저의 포인트를 충전하는 기능
     public UserPoint chargeUserPoint(long userId, long amount) {
 
         UserPoint userPoint = userPointTable.selectById(userId);
 
         if (amount <= 0) {
-            throw new IllegalArgumentException("충전금액은 음수 일 수 없습니다.");
+            throw new IllegalArgumentException("충전금액은 0원 이하 일 수 없습니다.");
         }
 
         if (userPoint.point() + amount >= 1_000_000) {
@@ -23,5 +27,17 @@ public class PointService {
         }
 
         return new UserPoint(userId,userPoint.point() + amount,System.currentTimeMillis());
+    }
+
+    // 특정 유저의 포인트를 조회하는 기능
+    public UserPoint getUserPoint(long userId) {
+
+        UserPoint userPoint = userPointTable.selectById(userId);
+
+        if (userPoint == null) {
+            throw new IllegalArgumentException("존재하지 않는 회원입니다.");
+        }
+
+        return userPoint;
     }
 }
--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -40,18 +40,18 @@ class PointServiceTest {
         assertEquals("충전 결과값이 1_000_000원을 넘을 수 없습니다.", exception.getMessage());
 
         // 메서드가 정확한 인자로 호출되었는지 검증
-       verify(pointService).chargeUserPoint(userId, invalidAmount);
+        verify(userPointTable).selectById(userId);
     }
 
     @Test
-    void 포인트_충전_시_충전금액은_음수_일_시_요청은_실패한다() {
+    void 포인트_충전_시_충전금액은_0원_이하_일시_요청은_실패한다() {
         long userId = 1L;
-        long invalidAmount = -1L;
+        long invalidAmount = 0L;
         
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> pointService.chargeUserPoint(userId, invalidAmount));
         
-        assertEquals("충전금액은 음수 일 수 없습니다.", exception.getMessage());
+        assertEquals("충전금액은 0원 이하 일 수 없습니다.", exception.getMessage());
     }
 
     @Test
@@ -70,5 +70,38 @@ class PointServiceTest {
 
         // selectById 메서드가 호출되었는지 검증
         verify(userPointTable).selectById(userId);
+    }
+    /**
+     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+     */
+    @Test
+    void 유저가_존재하지_않으면_포인트_조회_요청은_실패한다() {
+        long userId = 1L;
+
+        when(userPointTable.selectById(userId)).thenReturn(null);
+
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> pointService.getUserPoint(userId));
+
+        assertEquals("존재하지 않는 회원입니다.", exception.getMessage());
+    }
+
+    @Test
+    void 유저가_존재하면_포인트_조회_요청은_성공한다() {
+        long userId = 1L;
+        long amount = 100L;
+
+        UserPoint userPoint = new UserPoint(userId, amount, System.currentTimeMillis());
+
+        when(userPointTable.selectById(userId)).thenReturn(userPoint);
+
+        UserPoint result = pointService.getUserPoint(userId);
+
+        assertNotNull(result);
+        assertEquals(userId, result.id());
+        assertEquals(amount, result.point());
+
+        verify(userPointTable).selectById(userId);
+
     }
 }
