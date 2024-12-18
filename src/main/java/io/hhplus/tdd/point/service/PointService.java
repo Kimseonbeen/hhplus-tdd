4c1fea09d1769ac166aed673abc7881bca013adc
ksb9242 <ksb9242@gmail.com>
2024-12-18 10:29:19 +0900

    [refactor] : PointService 충전 메서드 UserPoint로 책임 분리

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -1,7 +1,9 @@
 package io.hhplus.tdd.point.service;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,15 +20,7 @@ public class PointService {
 
         UserPoint userPoint = userPointTable.selectById(userId);
 
-        if (amount <= 0) {
-            throw new IllegalArgumentException("충전금액은 0원 이하 일 수 없습니다.");
-        }
-
-        if (userPoint.point() + amount >= 1_000_000) {
-            throw new IllegalArgumentException("충전 결과값이 1_000_000원을 넘을 수 없습니다.");
-        }
-
-        return new UserPoint(userId,userPoint.point() + amount,System.currentTimeMillis());
+        return userPoint.chargePoint(amount);
     }
 
     // 특정 유저의 포인트를 조회하는 기능
